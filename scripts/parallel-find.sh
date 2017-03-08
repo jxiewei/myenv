4e7dd190f733bd29d0d451f3040fe983585ef412
xie wei <jxiewei@163.com>
2017-03-08 11:06:17 +0800

    Update parallel-find.sh

--- scripts/parallel-find.sh
+++ scripts/parallel-find.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find `pwd` -mindepth 1 -maxdepth 3 \( -path '*/.repo' -prune \) -o \( -name "$*" -print \)
-find `pwd` -mindepth 3 -maxdepth 3 \( \! -path '*/.repo' \! -path '*/.git' -type d -print \) | parallel -j32 find '{}' -name "$*"
+find `pwd` -mindepth 3 -maxdepth 3 \( \! -path '*/.repo' \! -path '*/.git' -type d -print \) | parallel -q -j32 find '{}' -name "$*"
